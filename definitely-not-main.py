c4a1b37479d0d0d170c1f92ffadb0d8bd35fee5a
Threx71 <gonzalo4795@gmail.com>
2023-07-17 17:07:49 -0400

    ñe

--- definitely-not-main.py
+++ definitely-not-main.py
@@ -12,26 +12,39 @@ load_dotenv(dotenv_path=env_path)
 
 DS_TOKEN = os.environ['DS_TOKEN']
 
+
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
+
+
+    channel=message.channel
+    reply_reference = message.reference or message.to_reference()
+    print(channel)
+    print(reply_reference)
+    
     if message.author == client.user:
         return
 
     if 'raro' in message.content:
         
         await message.add_reaction('🤔')
-        
+
+        #await message.channel.send('no deberia pasar', reference=reply_reference)
+
     return
 
-async def on_voice_state_update(member, before, after):
+#hacer votacion oculta par echar al woly con keywords, que sume y reste y podamos consultar que tan cerca estamos.
+
+#async def on_voice_state_update(member, before, after):
     # Check if the member is the specific user and the voice channel is the specific channel
-    if member.id == 175690952961294336 and after.channel.id == 1117264119939219488:
+    #if member.id == 175690952961294336 and after.channel.id == 1117264119939219488:
         
-        await member.guild.system_channel.send(f'{member.name} has joined {after.channel.name}!')
+        #await member.guild.system_channel.send(f'{member.name} has joined {after.channel.name}!')
 
 #welp
 client.run(DS_TOKEN)
